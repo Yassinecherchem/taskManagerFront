083029b49a7d5a788af925affd477c9704ab25fd
yassine cherchem <yassine.cherchem@emsi-edu.ma>
2023-12-18 19:32:07 +0100

    Demo home page

--- src/components/Lists.jsx
+++ src/components/Lists.jsx
@@ -3,6 +3,10 @@ import { Grid } from '@mui/material';
 import { styled } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
+import Draggable from 'react-draggable';
+import { useRef } from 'react';
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteIcon from '@mui/icons-material/Delete';
 import './Lists.css'
 
 const Item = styled(Paper)(({ theme }) => ({
@@ -14,18 +18,213 @@ const Item = styled(Paper)(({ theme }) => ({
   }));
 
 function Lists() {
+  const nodeRef = useRef(null);
   return (
     <Box sx={{ flexGrow: 1 }}>
     
         <Grid container spacing={2}>
         <Grid item xs={4}>
-            <Item>TODO</Item>
+            <Item >
+              <div style={{ fontWeight: 'bold'}}>
+              TODO
+              </div>
+              
+              <Draggable nodeRef={nodeRef}>
+                <Grid container spacing={2}>
+                    <Grid item xs={8}>
+                      Task
+                    </Grid>
+                    <Grid  item xs={4}>
+                      <div>
+                        <EditIcon fontSize="smaller"  />
+                        <DeleteIcon fontSize="smaller"/>
+                      </div>
+                    </Grid>
+                </Grid>
+                
+              </Draggable>
+              <Draggable nodeRef={nodeRef}> 
+                <Grid container spacing={2}>
+                    <Grid item xs={8}>
+                      Task
+                    </Grid>
+                    <Grid  item xs={4}>
+                      <div>
+                        <EditIcon fontSize="smaller"  />
+                        <DeleteIcon fontSize="smaller"/>
+                      </div>
+                    </Grid>
+                </Grid>
+                
+              </Draggable>
+              <Draggable nodeRef={nodeRef}>
+                <Grid container spacing={2}>
+                    <Grid  item xs={8}>
+                      Task 
+                    </Grid>
+                    <Grid  item xs={4}>
+                      <div>
+                        <EditIcon fontSize="smaller"  />
+                        <DeleteIcon fontSize="smaller"/>
+                      </div>
+                    </Grid>
+                </Grid>
+                
+              </Draggable>
+              <Draggable nodeRef={nodeRef}>
+                <div style={{textAlign:'left'}} ref={nodeRef}>
+                  TODO2 <br />
+                </div>
+                
+              </Draggable>
+              <Draggable nodeRef={nodeRef}>
+                <div style={{textAlign:'left'}} ref={nodeRef}>
+                  TODO3 <br />
+                </div>
+                
+              </Draggable>
+              <Draggable nodeRef={nodeRef}>
+                <div style={{textAlign:'left'}} ref={nodeRef}>
+                  TODO4 <br />
+                </div>
+                
+              </Draggable>
+              <Draggable nodeRef={nodeRef}>
+                <div style={{textAlign:'left'}} ref={nodeRef}>
+                  TODO5 <br />
+                </div>
+                
+              </Draggable>
+            </Item>
         </Grid>
         <Grid item xs={4}>
-            <Item>IN PROGRESS</Item>
+            <Item>
+              <div style={{ fontWeight: 'bold'}}>IN PROGRESS</div>
+              <Draggable nodeRef={nodeRef}>
+                <Grid container spacing={2}>
+                    <Grid item xs={8}>
+                      Task
+                    </Grid>
+                    <Grid  item xs={4}>
+                      <div>
+                        <EditIcon fontSize="smaller"  />
+                        <DeleteIcon fontSize="smaller"/>
+                      </div>
+                    </Grid>
+                </Grid>
+                
+              </Draggable>
+
+              <Draggable nodeRef={nodeRef}>
+                <Grid container spacing={2}>
+                    <Grid item xs={8}>
+                      Task
+                    </Grid>
+                    <Grid  item xs={4}>
+                      <div>
+                        <EditIcon fontSize="smaller"  />
+                        <DeleteIcon fontSize="smaller"/>
+                      </div>
+                    </Grid>
+                </Grid>
+                
+              </Draggable>
+
+              <Draggable nodeRef={nodeRef}>
+                <Grid container spacing={2}>
+                    <Grid item xs={8}>
+                      Task
+                    </Grid>
+                    <Grid  item xs={4}>
+                      <div>
+                        <EditIcon fontSize="smaller"  />
+                        <DeleteIcon fontSize="smaller"/>
+                      </div>
+                    </Grid>
+                </Grid>
+                
+              </Draggable>
+
+              <Draggable nodeRef={nodeRef}>
+                <Grid container spacing={2}>
+                    <Grid item xs={8}>
+                      Task
+                    </Grid>
+                    <Grid  item xs={4}>
+                      <div>
+                        <EditIcon fontSize="smaller"  />
+                        <DeleteIcon fontSize="smaller"/>
+                      </div>
+                    </Grid>
+                </Grid>
+                
+              </Draggable>
+            </Item>
         </Grid>
         <Grid item xs={4}>
-            <Item>DONE</Item>
+            <Item>
+            <div style={{ fontWeight: 'bold'}}>Done</div>
+
+            <Draggable nodeRef={nodeRef}>
+                <Grid container spacing={2}>
+                    <Grid item xs={8}>
+                      Task
+                    </Grid>
+                    <Grid  item xs={4}>
+                      <div>
+                        <EditIcon fontSize="smaller"  />
+                        <DeleteIcon fontSize="smaller"/>
+                      </div>
+                    </Grid>
+                </Grid>
+                
+              </Draggable>
+
+              <Draggable nodeRef={nodeRef}>
+                <Grid container spacing={2}>
+                    <Grid item xs={8}>
+                      Task
+                    </Grid>
+                    <Grid  item xs={4}>
+                      <div>
+                        <EditIcon fontSize="smaller"  />
+                        <DeleteIcon fontSize="smaller"/>
+                      </div>
+                    </Grid>
+                </Grid>
+                
+              </Draggable>
+
+              <Draggable nodeRef={nodeRef}>
+                <Grid container spacing={2}>
+                    <Grid item xs={8}>
+                      Task
+                    </Grid>
+                    <Grid  item xs={4}>
+                      <div>
+                        <EditIcon fontSize="smaller"  />
+                        <DeleteIcon fontSize="smaller"/>
+                      </div>
+                    </Grid>
+                </Grid>
+                
+              </Draggable>
+
+              <Draggable nodeRef={nodeRef}>
+                <Grid container spacing={2}>
+                    <Grid item xs={8}>
+                      Task
+                    </Grid>
+                    <Grid  item xs={4}>
+                      <div>
+                        <EditIcon fontSize="smaller"  />
+                        <DeleteIcon fontSize="smaller"/>
+                      </div>
+                    </Grid>
+                </Grid>
+                
+              </Draggable>
+            </Item>
         </Grid>
         </Grid>
 
